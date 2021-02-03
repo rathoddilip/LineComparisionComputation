47c488f012ad179c0357d16005c537836b66c4de
diliprathod32 <diliprathod32@gmail.com>
2021-02-03 12:16:55 +0530

    [Dilip] WelcomeToLineComaprision

---
+++ LineComparision/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LineComparision
+{
+    /// <summary>
+    /// /
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparision computation");
+        }
+    }
+}
